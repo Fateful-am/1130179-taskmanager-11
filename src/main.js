79d9989512bf9ee292ab9c19168b764513ee4d00
Fateful-am <fateful.am@gmail.com>
2020-04-14 22:25:22 +0300

    Вынесет отрисовку задач в отдельные функции

--- src/main.js
+++ src/main.js
@@ -15,45 +15,15 @@ const TASK_COUNT = 22;
 const SHOWING_TASKS_COUNT_ON_START = 8;
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
+const renderTask = () => {};
+
+const renderBoard = () => {};
+
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 const filters = generateFilters();
 const tasks = generateTasks(TASK_COUNT);
-// console.log(tasks);
 
-// Отрисовка меню
-render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
-// Отрисовка фильтров
-render(siteMainElement, createFilterTemplate(filters), `beforeend`);
-// Отрисовка доски
-render(siteMainElement, createBoardTemplate(), `beforeend`);
-
-const taskListElement = siteMainElement.querySelector(`.board__tasks`);
-const boardElement = siteMainElement.querySelector(`.board`);
-
-// Отрисовка формы
-render(taskListElement, createTaskEditTemplate(tasks[0]), `beforeend`);
-// Отрисовка сортировки
-render(boardElement, createSortTemplate(), `afterbegin`);
-
-let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
-// Отрисовка задач
-tasks.slice(1, showingTasksCount)
-  .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
-
-// Отрисовка кнопки "load more"
-render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
-const loadMoreButton = boardElement.querySelector(`.load-more`);
-
-loadMoreButton.addEventListener(`click`, () => {
-  const prevTasksCount = showingTasksCount;
-  showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
-
-  tasks.slice(prevTasksCount, showingTasksCount)
-    .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
-
-  if (showingTasksCount >= tasks.length) {
-    loadMoreButton.remove();
-  }
-});
+render(siteHeaderElement, new SiteMenuComponent().getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, new FilterComponent(filters).getElement(), RenderPosition.BEFOREEND);
