abdae4209cb16ab76d458d89eaebbc994a75009c
Fateful-am <fateful.am@gmail.com>
2020-04-18 17:16:09 +0300

    Добавит в компонент `load-more` метод для подписки на клик

--- src/components/load-more-button.js
+++ src/components/load-more-button.js
@@ -7,4 +7,12 @@ export default class LoadMoreButton extends AbstractComponent {
   getTemplate() {
     return `<button class="load-more" type="button">load more</button>`;
   }
+
+  /**
+   * Подписка кнопки на клик
+   * @param {function} handler - Обработчик клика по кнопке
+   */
+  setClickHandler(handler) {
+    this.getElement().addEventListener(`click`, handler);
+  }
 }
